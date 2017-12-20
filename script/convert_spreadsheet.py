6cfc68b03713e2c2dda2e744e7f7cc724134baf2
minhkv <khongvanminhpfiev@gmail.com>
2017-12-20 14:18:18 +0700

    add convert

--- script/convert_spreadsheet.py
+++ script/convert_spreadsheet.py
@@ -1,4 +1,11 @@
 from __future__ import print_function
+import os
 import pandas as pd
-spread_sheet_folder = "/media/minhkv/Data/HocTap/DaiHoc/MasterI/datasets_mica/Annotation_dataset"
-df = pd.read_excel()
+project_folder = "/media/minhkv/Data/HocTap/DaiHoc/MasterI/datasets_mica/Annotation_dataset"
+spread_sheet_folder = os.path.join(project_folder, "spreadsheet")
+name = "20171123_Hung_lan1_23-11-2017__11-05-57"
+path = os.path.join(spread_sheet_folder, name + ".xlsx")
+xl = pd.ExcelFile(path)
+print(xl.sheet_names)
+df = pd.read_excel(path)
+print(df.head)
